03a9919da9543ac6877e6a8515f73fb0288f6988
khalid karam <khalid.tasks@gmail.com>
2024-12-07 22:34:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LinqElementOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 1, 5, 1, 8, 5 };
+
+            var elements = numbers.Where(c => c > 90).DefaultIfEmpty(50);
+
+            foreach (var item in elements)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
